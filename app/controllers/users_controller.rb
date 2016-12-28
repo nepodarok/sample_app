e5ef25c491a6cd38f4ab01c48639fadc17d83c1b
nepodarok <aleksandrnepodarok@gmail.com>
2016-12-28 10:13:07 +0600

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ before_action :admin_user,     only: :destroy
 
   def show
     @user = User.find(params[:id])
-    # debugger
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def create
@@ -56,15 +56,6 @@ before_action :admin_user,     only: :destroy
     end
 
     # Before filters
-
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
 
     # Confirms the correct user.
    def correct_user
